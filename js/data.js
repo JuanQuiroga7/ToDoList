1afb2b1af6b4b6514fee83d4ce48e57856582d61
JuanQuiroga7 <160855455+JuanQuiroga7@users.noreply.github.com>
2024-06-23 20:37:21 -0500

    feat: :sparkles: Funcion eliminar task agregado
    Se realiza funcion que permite al usuario borrar el task al clickear en el trash can. Se elimina tanto del DOM como del servidor.

--- js/data.js
+++ js/data.js
@@ -22,3 +22,5 @@ export function fetchTasks() {
       method: 'DELETE',
     });
   }
+
+  
--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-import { fetchTasks, postTask, deleteTask } from './data.js';
+import { fetchTasks, postTask, deleteTask as deleteTaskFromServer } from './data.js';
 
 
 fetchTasks().then(tasks => {
@@ -11,32 +11,52 @@ fetchTasks().then(tasks => {
     container.innerHTML = '';
   
     tasks.forEach(task => {
-      const taskHTML = `
-        <li>
-            <span>${task.task}</span>
-            <div>
-                <div class="circular-background">
-                    <img src="storage/img/check.png" onclick="markAsDone(this)" />
-                </div>
-                <div class="circular-background">
-                    <img src="storage/img/trash-can.png" onclick="deleteTask(this)" />
-                </div>
-            </div>
-        </li>
-    `;
+      const li = document.createElement('li');
+      li.setAttribute('data-id', task.id);
   
-      container.innerHTML += taskHTML;
+      li.innerHTML = `
+        <span>${task.task}</span>
+        <div>
+          <div class="circular-background">
+            <img src="storage/img/check.png" onclick="markAsDone(this)" />
+          </div>
+          <div class="circular-background">
+            <img src="storage/img/trash-can.png" />
+          </div>
+        </div>
+      `;
+  
+      li.querySelector('img[src="storage/img/trash-can.png"]').addEventListener('click', function() {
+        deleteTask(this);
+      });
+  
+      container.appendChild(li);
     });
+  }
+
+  
+function deleteTask(imgElement) {
+    
+    const taskElement = imgElement.closest('li');
+  
+    // Get the unique ID of the task from the 'data-id' attribute
+    const taskId = taskElement.getAttribute('data-id');
+  
+    
+    taskElement.remove();
+  
+    
+    deleteTaskFromServer(taskId)
+      .then(() => {
+        console.log(`Task ${taskId} eliminado del servidor`);
+      })
+      .catch(error => {
+        console.error(`Error al eliminar ${taskId}: `, error);
+      });
   }
 
 // // Post a new task
 // postTask('New Task', 'ready').then(task => {
 //   console.log(`Posted new task with ID: ${task.id}`);
 //   // Here you can also update your UI to include the new task
-// });
-
-// // Delete a task
-// deleteTask(1).then(() => {
-//   console.log('Deleted task with ID: 1');
-//   // Here you can also update your UI to remove the deleted task
 // });
